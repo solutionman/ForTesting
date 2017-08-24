6aaa42719a16082dceb33a5423c76e324042893a
lom <solutionman@inbox.ru>
2017-08-24 16:18:27 +0700

    testing writing char[] to file

--- src/TestingBigDecimal.java
+++ src/TestingBigDecimal.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.math.BigDecimal;
 
 // String to BigDecimal and Back to String
@@ -17,12 +20,39 @@ public class TestingBigDecimal {
 
         StringBuilder sb = new StringBuilder();
 
-        sb.append(b);                         // append  - adds strings to our string
+        sb.append(c);                         // append  - adds strings to our string
         System.out.println(sb);
         sb.append(',');
         System.out.println(sb);
 
+        BigDecimal zero = BigDecimal.ZERO;
+        System.out.println("BigDecimal zero = " + zero);
+
+        String StringData = sb.toString();
+
+        //char[] data = {'0', '.', '4', '0'};
+        char[] data = StringData.toCharArray();
+
+        try {
+            File date = new File("./output");
+            PrintWriter writer = new PrintWriter("12345.s09");
+            writer.write(data);
+            writer.close();
+        } catch (IOException e){
+            System.out.println("Some problems with writing to file");
+        }
+
     }
 }
+
+
+
+
+
+
+
+
+
+
 
 
